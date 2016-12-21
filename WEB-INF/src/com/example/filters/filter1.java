b7872368612bb745ea9d8868265716639a818f62
andrey_bespaliy <andrey_bespaliy@csltd.com.ua>
2016-12-21 09:47:15 +0200

    Сделал запись лога в файл.

--- WEB-INF/src/com/example/filters/filter1.java
+++ WEB-INF/src/com/example/filters/filter1.java
@@ -9,9 +9,12 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class filter1 implements Filter {
     private FilterConfig fc;
+    private String LogFileName;
 
     public void init(FilterConfig config) throws ServletException {
         this.fc = config;
+        LogFileName = this.fc.getInitParameter("LogFileName");
+        System.out.print("\nOne two three\n\n");
     }
 
     public void doFilter(ServletRequest req,
@@ -20,13 +23,25 @@ public class filter1 implements Filter {
 
         HttpServletRequest httpReq = (HttpServletRequest) req;
         String name = httpReq.getRemoteUser();
-        if (name != name) {
-            fc.getServletContext().log("User " + name + " is updating");
+        if (name != null) {
+            //fc.getServletContext().log("User " + name + " is updating"); // it is don't work (don't know why)
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(LogFileName, true);
+                writer.write("User " + name + " visited a secret page\r\n");
+            }
+            catch(IOException e ){
+                System.out.print(e.getMessage());
+            }
+            finally{
+                writer.close();
+            }
         }
         chain.doFilter(req, resp);
     }
 
     public void destroy() {
-        // si cleanup stuff
+        fc = null;
+        LogFileName = null;
     }
 }
